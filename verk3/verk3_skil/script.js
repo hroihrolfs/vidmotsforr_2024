c260f5f5b22cf813044f4405e81b0969efc2cff3
hroihrolfs <89214090+hroihrolfs@users.noreply.github.com>
2024-04-09 14:30:15 +0000

    ok

---
+++ verk3/verk3_skil/script.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// const controls = new OrbitControls( camera, renderer.domElement );  // þarf að færa eftir að cameran erbúin til.
+// const loader = new GLTFLoader();
+
+// allt fyrir ofan er import og add-ons.
+
+
+// cameran og rendera inn.
+const scene = new THREE.Scene();    // 75 er fov (degrees), aspect ratio, near og far (nálægasti punktur og lengsti í burt.)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// renderinn er webgl og setjum hann í stærðina á glugganum og bætum honum sem child við body.
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+
+// material 
+const loader = new THREE.CubeTextureLoader();
+loader.setPath( "/verk3_skil/floor.jpg" );
+const textureCube = loader.load( [
+    "floor.png", "floor.png",
+    "floor.png", "floor.png",
+    "floor.png", "floor.png"
+] );
+
+// cube fyrir neðan"
+// default shape cube.                  x  y  z
+const geometry = new THREE.BoxGeometry( 10, 1, 1 ); // stærð
+const material = new THREE.MeshStandardMaterial( { color: 0xffffff, envMap: textureCube } ); // litur
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+console.log("tengt");
+
+// loopa sem er alltaf að rendera og updatea.
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera);
+}
+
+animate();
